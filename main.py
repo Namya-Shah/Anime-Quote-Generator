38780d82dce9cc24828640bdc9220b97e228a69b
Namya Shah <85009103+Namya-Shah@users.noreply.github.com>
2025-05-20 21:46:25 +0530

    Modified

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ import streamlit as st
 import random
 import pandas as pd
 
-def quote_generator():
-    i = random.randint(1, 122)
-    quote = st.markdown(f"Quote: **{quotes[i]}**")
-    char = st.markdown(f"Character: **{character[i]}**")
-    annime = st.markdown(f"Anime: **{anime[i]}**")
-    return quote, char, annime
-
 st.title("Anime Motivator")
 
 data = pd.read_csv("data.csv")
@@ -19,13 +12,13 @@ anime = data["Anime"]
 
 if 'index' not in st.session_state:
     st.session_state.index = 0
-    
+
+st.markdown(f"Quote: **{quotes[st.session_state.index]}**")
+st.markdown(f"Character: **{character[st.session_state.index]}**")
+st.markdown(f"Anime: **{anime[st.session_state.index]}**")
+
 if st.button("Show Next"):
     if st.session_state.index < len(quotes) - 1:
         st.session_state.index += 1
     else:
         st.session_state.index = 0
-
-st.markdown(f"Quote: **{quotes[st.session_state.index]}**")
-st.markdown(f"Character: **{character[st.session_state.index]}**")
-st.markdown(f"Anime: **{anime[st.session_state.index]}**")
